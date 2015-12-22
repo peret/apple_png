cfdc1834fa30092b34662e1eaa8c0e5e2bf1c799
Peter Retzlaff <peter@playtestcloud.com>
2015-12-22 13:42:05 +0100

    Wasn't such a good idea to remove the extension, after all.

--- lib/apple_png.rb
+++ lib/apple_png.rb
@@ -14,7 +14,25 @@ class ApplePng
   # Get the PNG data as string. The conversion from Apple PNG data to standard PNG data will be performed when this method is first called.
   # @return [String] Binary string containing standard PNG data
   def data
-    @data = self.convert_apple_png(@raw_data) if @data.nil?
-    return @data
+    @data = convert_apple_png if @data.nil?
+    @data
+  end
+
+  def convert_apple_png
+    Dir.mktmpdir do |tmpdir|
+      apple_png_file = File.join(tmpdir, 'apple.png')
+      File.open(apple_png_file, 'wb') do |file|
+        file.write(@raw_data)
+      end
+
+      result_file = File.join(tmpdir, 'uncrushed.png')
+      uncrush_cmd = "xcrun -sdk iphoneos pngcrush -revert-iphone-optimizations #{apple_png_file} #{result_file}"
+
+      pid, _, _, _ = Open4.popen4(uncrush_cmd)
+      _, status = Process.waitpid2 pid
+      return nil if status.exitstatus != 0
+
+      File.read(result_file)
+    end
   end
 end
